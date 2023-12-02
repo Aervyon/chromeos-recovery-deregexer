1ffc329e6f08975ea2df543ce74062adce907e4d
Aervyon <aervy@aervyon.com>
2023-12-02 00:32:16 -0600

    well if this isn't insanity i don't know what is

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Steps:
 
-1 build with go 1.21 or later
-2 place `recovery2.json` into the same directory as executable
-3 run command
-4 profit
+1) build with go 1.21 or later
+2) place `recovery2.json` into the same directory as executable
+3) run command
+4) profit
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"encoding/json"
 	"fmt"
 	"io"
@@ -28,6 +29,7 @@ func main() {
 	start := regexp.MustCompile(`^\^|^\(`)
 	end := regexp.MustCompile(`-.{0,2}.$|\..*|\\.*$| \(.*$|\s$`)
 	end2 := regexp.MustCompile(`\?$|\[.*$`)
+	fuckparens := regexp.MustCompile(`^\(`)
 
 	json.Unmarshal(bytes, &recovery)
 	for i := 0; i < len(recovery); i++ {
@@ -36,10 +38,40 @@ func main() {
 		recovery[i].HwIDMatch = end.ReplaceAllString(recovery[i].HwIDMatch, "")
 		recovery[i].HwIDMatch = end2.ReplaceAllString(recovery[i].HwIDMatch, "")
 		recovery[i].HwIDMatch = end.ReplaceAllString(recovery[i].HwIDMatch, "")
+		recovery[i].HwIDMatch = fuckparens.ReplaceAllString(recovery[i].HwIDMatch, "")
 		fmt.Println(recovery[i].HwIDMatch)
 	}
 
 	fmt.Println("Modified", len(recovery), "models in", (time.Since(now)))
+
+	// TURN INTO CSV
+	csvFile, err := os.Create("recovery2-deregexd.csv")
+	if err != nil {
+		fmt.Println("Failed to create CSV. Will continue to save to JSON")
+	}
+	defer csvFile.Close()
+
+	// hwidmatch manufacturer md5 model url chrome_version
+	if err == nil {
+		writer := csv.NewWriter(csvFile)
+		defer writer.Flush()
+
+		header := []string{"hwidmatch", "manufacturer", "md5", "model", "url", "chrome_version"}
+		if err := writer.Write(header); err != nil {
+			fmt.Println("Fuckin failed to write CSV header. 10/10 would give head again. See below")
+			fmt.Println(err)
+		} else {
+			for _, r := range recovery {
+				var csvRow []string
+				csvRow = append(csvRow, r.HwIDMatch, r.Manufacturer, r.MD5, r.Model, r.URL, r.Chromeversion)
+				if err := writer.Write(csvRow); err != nil {
+					fmt.Println("Encountered Error. See below")
+					fmt.Println(err)
+				}
+			}
+		}
+	}
+
 	fmt.Println("Saving to recovery2-deregexd.json")
 
 	endBytes, err := json.MarshalIndent(recovery, "", "  ")
@@ -50,14 +82,15 @@ func main() {
 	}
 
 	os.WriteFile("recovery2-deregex.json", endBytes, 0666)
+	fmt.Println("Saving to recovery2-deregexd.csv")
 }
 
 type Recovery struct {
-	SHA1          string `json:"sha1"`
 	HwIDMatch     string `json:"hwidmatch"`
 	Model         string `json:"model"`
 	URL           string `json:"url"`
 	Chromeversion string `json:"chrome_version"`
 	Manufacturer  string `json:"manufacturer"`
 	Version       string `json:"version"`
+	MD5           string `json:"md5"`
 }
