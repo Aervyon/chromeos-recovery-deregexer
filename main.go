da6acf9d818ec6ddf498a78c71960d974e9310ce
Aervyon <aervy@aervyon.com>
2023-12-02 01:28:22 -0600

    feat: add more metrics, remove logging

--- main.go
+++ main.go
@@ -11,7 +11,6 @@ import (
 )
 
 func main() {
-	now := time.Now()
 	fmt.Println("Opening file recovery2.json")
 	file, err := os.Open("recovery2.json")
 
@@ -23,15 +22,17 @@ func main() {
 	defer file.Close()
 
 	bytes, _ := io.ReadAll(file)
+
 	var recovery []Recovery
 
+	json.Unmarshal(bytes, &recovery)
+	now := time.Now()
 	// spaces := regexp.MustCompile(`\s.*$`)
 	start := regexp.MustCompile(`^\^|^\(`)
 	end := regexp.MustCompile(`-.{0,2}.$|\..*|\\.*$| \(.*$|\s$`)
 	end2 := regexp.MustCompile(`\?$|\[.*$`)
 	fuckparens := regexp.MustCompile(`^\(`)
 
-	json.Unmarshal(bytes, &recovery)
 	for i := 0; i < len(recovery); i++ {
 		// recovery[i].HwIDMatch = spaces.ReplaceAllString(recovery[i].HwIDMatch, "")
 		recovery[i].HwIDMatch = start.ReplaceAllString(recovery[i].HwIDMatch, "")
@@ -39,37 +40,19 @@ func main() {
 		recovery[i].HwIDMatch = end2.ReplaceAllString(recovery[i].HwIDMatch, "")
 		recovery[i].HwIDMatch = end.ReplaceAllString(recovery[i].HwIDMatch, "")
 		recovery[i].HwIDMatch = fuckparens.ReplaceAllString(recovery[i].HwIDMatch, "")
-		fmt.Println(recovery[i].HwIDMatch)
+		// fmt.Println(recovery[i].HwIDMatch)
 	}
 
-	fmt.Println("Modified", len(recovery), "models in", (time.Since(now)))
+	fmt.Println("Modified", len(recovery), "chromebook models in", (time.Since(now)))
 
-	// TURN INTO CSV
-	csvFile, err := os.Create("recovery2-deregexd.csv")
+	success, err := toCSV(recovery, "recovery2-deregexd.csv")
+
 	if err != nil {
-		fmt.Println("Failed to create CSV. Will continue to save to JSON")
+		fmt.Println("Something went wrong while converting to CSV. See error below. (Will try to save as JSON)")
+		fmt.Println(err)
 	}
-	defer csvFile.Close()
-
-	// hwidmatch manufacturer md5 model url chrome_version
-	if err == nil {
-		writer := csv.NewWriter(csvFile)
-		defer writer.Flush()
-
-		header := []string{"hwidmatch", "manufacturer", "md5", "model", "url", "chrome_version"}
-		if err := writer.Write(header); err != nil {
-			fmt.Println("Fuckin failed to write CSV header. 10/10 would give head again. See below")
-			fmt.Println(err)
-		} else {
-			for _, r := range recovery {
-				var csvRow []string
-				csvRow = append(csvRow, r.HwIDMatch, r.Manufacturer, r.MD5, r.Model, r.URL, r.Chromeversion)
-				if err := writer.Write(csvRow); err != nil {
-					fmt.Println("Encountered Error. See below")
-					fmt.Println(err)
-				}
-			}
-		}
+	if success {
+		fmt.Println("Saved to recovery2-deregexd.csv")
 	}
 
 	fmt.Println("Saving to recovery2-deregexd.json")
@@ -82,7 +65,36 @@ func main() {
 	}
 
 	os.WriteFile("recovery2-deregex.json", endBytes, 0666)
-	fmt.Println("Saving to recovery2-deregexd.csv")
+	fmt.Println("Finished in", time.Since(now))
+}
+
+func toCSV(data []Recovery, destination string) (success bool, err error) {
+	// TURN INTO CSV
+	csvFile, err := os.Create(destination)
+	if err != nil {
+		return false, fmt.Errorf("failed to create CSV.")
+	}
+	defer csvFile.Close()
+
+	// hwidmatch manufacturer md5 model url chrome_version
+	if err == nil {
+		writer := csv.NewWriter(csvFile)
+		defer writer.Flush()
+
+		header := []string{"hwidmatch", "manufacturer", "md5", "model", "url", "chrome_version"}
+		if err := writer.Write(header); err != nil {
+			return false, err
+		}
+
+		for _, r := range data {
+			var csvRow []string
+			csvRow = append(csvRow, r.HwIDMatch, r.Manufacturer, r.MD5, r.Model, r.URL, r.Chromeversion)
+			if err := writer.Write(csvRow); err != nil {
+				return false, err
+			}
+		}
+	}
+	return true, nil
 }
 
 type Recovery struct {
